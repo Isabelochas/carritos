e7368922561bac33e37a0f3cc4c57ddb47e7f694
Isabelochas <92957419+Isabelochas@users.noreply.github.com>
2022-04-07 17:14:57 -0500

    Update Form.js

--- Form.js
+++ Form.js
@@ -2,7 +2,7 @@ class Form {
   constructor() {
     this.input = createInput("").attribute("placeholder", "Ingresa tu nombre");
     this.playButton = createButton("Jugar");
-    this.titleImg = createImg("./assets/title.png", "game title");
+    this.titleImg = createImg("title.png", "game title");
     this.greeting = createElement("h2");
   }
 
